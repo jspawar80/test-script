7a2f89f003ac537e7cdcafffe8f45644c8fa6ebd
jspawar80 <127867978+jspawar80@users.noreply.github.com>
2023-07-16 01:53:38 +0530

    Update task-def.sh

--- task-def.sh
+++ task-def.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-set -e
-
-# $1 is the new image tag
-newImage="$1"
-# $2 is the file path of the task definition json
-taskDefPath="$2"
-
-taskDef=$(cat $taskDefPath)
-
-# using jq to replace the image
-newTaskDef=$(echo $taskDef | jq --arg newImage "$newImage" '.containerDefinitions[0].image = $newImage')
-
-# write back to file
-echo "$newTaskDef" > $taskDefPath
+ls -al
