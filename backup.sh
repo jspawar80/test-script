a64f63bb3aea62ef7303384207127a5116e009dd
jspawar80 <127867978+jspawar80@users.noreply.github.com>
2023-08-01 11:11:29 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -20,16 +20,26 @@ DATE=$(date +%Y%m%d%H%M%S)
 DUMP_FILE="$RDS_DB"_"$DATE".sql
 
 # SSH to the bastion host and execute pg_dump
-ssh -v -i $PRIVATE_KEY_PATH -l $BASTION_USER $BASTION_IP "PGPASSWORD=<your-rds-password> pg_dump -h $RDS_HOST -U $RDS_USER -F p -f /tmp/$DUMP_FILE -d $RDS_DB"
+ssh -v -i $PRIVATE_KEY_PATH -l $BASTION_USER $BASTION_IP "PGPASSWORD=lynk_api1234! pg_dump -h $RDS_HOST -U $RDS_USER -F p -f /tmp/$DUMP_FILE -d $RDS_DB"
 
 # Copy the dump file from bastion host to the local machine
 scp -v -i $PRIVATE_KEY_PATH $BASTION_USER@$BASTION_IP:/tmp/$DUMP_FILE $LOCAL_DIR
 
 # Copy the dump file from local machine into the Docker container
-docker cp $LOCAL_DIR/$DUMP_FILE $DOCKER_CONTAINER:/tmp/
+docker cp $LOCAL_DIR/$DUMP_FILE $DOCKER_CONTAINER:/tmp/$DUMP_FILE
+
+
+# Drop the existing database and create a new one
+#docker exec -it $DOCKER_CONTAINER psql -h $LOCAL_RDS_HOST -U $LOCAL_RDS_USER -c "DROP DATABASE IF EXISTS $LOCAL_RDS_DB; CREATE DATABASE $LOCAL_RDS_DB;"
+docker exec -it $DOCKER_CONTAINER dropdb -h $LOCAL_RDS_HOST -U $LOCAL_RDS_USER --if-exists $LOCAL_RDS_DB
+
+
+# Create a new database
+docker exec -it $DOCKER_CONTAINER createdb -h $LOCAL_RDS_HOST -U $LOCAL_RDS_USER $LOCAL_RDS_DB
+
 
 # Restore the database using the dump file
-docker exec -it $DOCKER_CONTAINER psql -h LOCAL_RDS_HOST -U LOCAL_RDS_USER -d LOCAL_RDS_DB -f /tmp/$DUMP_FILE
+docker exec -it $DOCKER_CONTAINER psql -h $LOCAL_RDS_HOST -U $LOCAL_RDS_USER -d $LOCAL_RDS_DB -f /tmp/$DUMP_FILE
 
 # Remove the dump file from the bastion host
 ssh -v -i $PRIVATE_KEY_PATH -l $BASTION_USER $BASTION_IP "rm -f /tmp/$DUMP_FILE"
